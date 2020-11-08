d9cade0f67b2d63ccae2b6bedcd0de3344665925
Lenard Frommelt <lenny09918050@googlemail.com>
2020-11-08 13:04:32 +0100

    adjusted to be a reasonable recipe for package thingies

--- individualize.js
+++ individualize.js
@@ -2,5 +2,5 @@ const exec = require("child_process").exec
 const pathModule = require("path")
 
 var toolsetDir = pathModule.resolve(process.cwd(), "toolset")
-var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForCli.pl")
-exec(scriptPath, {cwd:toolsetDir}, (err, out, stderr) => console.log(out + "\nindividualized cli!"))
+var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForPackage.pl")
+exec(scriptPath, {cwd:toolsetDir}, (err, out, stderr) => console.log(out + "\nindividualized package!"))
